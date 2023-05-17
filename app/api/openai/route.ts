e20dd636220e8b97a1bf5a133737669913369790
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-05-17 09:29:59 -0500

    first commit

---
+++ app/api/openai/route.ts
@@ -0,0 +1,56 @@
+import { NextResponse } from 'next/server';
+import { AxiosResponse } from 'axios';
+
+//
+
+import { Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function POST(request: Request) {
+  try {
+    const { title, role } = await request.json();
+    const aiRes: AxiosResponse<CreateChatCompletionResponse, any> = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: `Create a dungeons and dragons character origin story with html tags from this title: ${title}`,
+        },
+        {
+          role: 'system',
+          content: `${role || 'I am a helpful assistant'}. Write with html tags`,
+        },
+      ],
+    });
+    return NextResponse.json(
+      {
+        content: aiRes.data.choices[0].message?.content,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('req error', error);
+    return NextResponse.json({ error: 'post did not update' }, { status: 500 });
+  }
+}
+
+/**
+ * BOTTLENECK OPTION FOR LIMITING REQUESTS
+ */
+
+// import Bottleneck from 'bottleneck';
+// const limiter = new Bottleneck({
+//   maxConcurrent: 1, // Only allow 1 request at a time
+//   minTime: 2000, // Wait at least 2 seconds between requests
+//   reservoir: 3, // Allow up to 3 requests in the reservoir
+//   reservoirRefreshInterval: 60 * 1000, // Refresh the reservoir every minute
+// });
+// content: `Create a 3 line blog post with html tags based on this title: ${title}`,
+
+/**
+ * Add limiter to api request by wrapping it around the request
+ */
