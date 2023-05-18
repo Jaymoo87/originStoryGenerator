0fc66f562e319cbdac8c7c421a7b77993a515d8a
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-05-18 13:52:47 -0500

    homeland data in there, now to send promts to gpt

--- app/(home)/Character.tsx
+++ app/(home)/Character.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { characterOptions } from '@/public/data/CharacterData';
 
 import Select from 'react-select';
@@ -8,39 +8,39 @@ import Select from 'react-select';
 type Props = {};
 
 const Character = (props: Props) => {
+  const { raceOptions, classOptions, homelandOptions } = characterOptions;
+
   return (
     <form action="" className="flex justify-center ">
-      <div className="grid w-1/5 grid-cols-1 grid-rows-1 p-2 m-3 rounded-lg bg-slate-500">
+      <div className="grid w-1/5 grid-cols-1 grid-rows-1 p-2 m-3 rounded-lg ">
         <label htmlFor="name" className="mx-3">
           name
         </label>
-        <input className="p-1 mx-2 mb-4 rounded-lg " placeholder="Name" id="name" />
+        <input className="p-1 mx-2 mb-4 rounded-lg shadow-xl " placeholder="Name" id="name" />
 
         <label className="mx-3" htmlFor="age">
           age
         </label>
-        <input className="p-1 mx-2 mb-4 rounded-lg " placeholder="Age" id="age" />
+        <input className="p-1 mx-2 mb-4 rounded-lg shadow-xl" placeholder="Age" id="age" />
 
         <label className="mx-3" htmlFor="race">
           race
         </label>
         <Select
-          options={characterOptions.raceOptions}
+          options={raceOptions}
           getOptionLabel={(option) => `${option.label} --> ${option.special}`}
-          className="p-1 mx-2 mb-4 font-bold text-black rounded-lg"
+          className="p-1 mx-2 mb-4 font-bold text-black shadow-xl"
         />
 
         <label className="mx-3">class</label>
         <Select
-          options={characterOptions.classOptions}
+          options={classOptions}
           getOptionLabel={(option) => `${option.label} --> ${option.special}`}
-          className="p-1 mx-2 mb-4 font-bold text-black rounded-lg "
+          className="p-1 mx-2 mb-4 font-bold text-black shadow-xl"
         />
 
         <label className="mx-3"> homeland </label>
-        <select className="p-1 mx-2 mb-4 rounded-lg">
-          <option value="homeland">select a homeland</option>
-        </select>
+        <Select options={homelandOptions} className="p-1 mx-2 mb-4 font-bold text-black shadow-xl" />
       </div>
     </form>
   );
