9a91dc8f7325eade94ab3b527b8801bb9aefea10
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-05-21 15:55:55 -0500

    CORS fix

--- app/(shared)/CharacterOriginStory.tsx
+++ app/(shared)/CharacterOriginStory.tsx
@@ -43,7 +43,6 @@ const CharacterOriginStory = ({ characterName, age, race, characterClass, homela
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Access-Control-Allow-Origin': 'https://origin-story-generator.vercel.app/',
       },
       body: JSON.stringify({
         characterName,
