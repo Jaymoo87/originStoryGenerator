86a290b8833c1cae09998465c538963055c04e61
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-05-22 16:58:13 -0500

    next.config should be fixed now

--- app/(shared)/CharacterOriginStory.tsx
+++ app/(shared)/CharacterOriginStory.tsx
@@ -38,7 +38,7 @@ const CharacterOriginStory = ({ characterName, age, race, characterClass, homela
 
     // NEXT_PUBLIC_URL for devlopment
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/openai`, {
+    const response = await fetch(`/api/openai`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
