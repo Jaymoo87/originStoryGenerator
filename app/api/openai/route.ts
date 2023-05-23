0efe5e6b7b8d2957ff0a7615099475c81dd2d3fc
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-05-23 13:30:50 -0500

    trying to workout some of the API issues for deployment, development works great

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { AxiosResponse } from 'axios';
 
-import { Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai';
+import {
+  ChatCompletionResponseMessageRoleEnum,
+  Configuration,
+  CreateChatCompletionRequest,
+  CreateChatCompletionResponse,
+  OpenAIApi,
+} from 'openai';
 
 import NextCors from 'nextjs-cors';
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -9,49 +15,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-// async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   await NextCors(req, res, {
-//     methods: ['POST'],
-//     origin: '*',
-//     optionsSuccessStatus: 200,
-//   });
-
-//   try {
-//     const openai = new OpenAIApi(configuration);
-
-//     if (req.method === 'POST') {
-//       // Handle the POST request
-//       ///  add different variables for character stats and other information to send to chatgpt request. ex: {var1, var2, role}
-//       const { role, characterName, characterClass, age, race, homeland } = await req.body;
-//       const aiRes: AxiosResponse<CreateChatCompletionResponse, any> = await openai.createChatCompletion({
-//         model: 'gpt-3.5-turbo',
-//         messages: [
-//           {
-//             role: 'user',
-//             content: `Create a 5 paragraph dungeons and dragons character origin story for ${characterName}, a ${age} year old ${race} ${characterClass} from ${homeland} with html tags `,
-//           },
-//           {
-//             role: 'system',
-//             content: `${role || 'I am a helpful assistant'}. Write with html tags`,
-//           },
-//         ],
-//       });
-//       return NextResponse.json(
-//         {
-//           content: aiRes.data.choices[0].message?.content,
-//         },
-//         {
-//           status: 200,
-//         }
-//       );
-//     }
-//   } catch (error) {
-//     console.error('req error', error);
-//     return NextResponse.json({ error: 'post did not update' }, { status: 500 });
-//   }
-//   // Rest of the API logic
-//   res.json({ message: 'Hello NextJs Cors!' });
-// }
 
 export async function POST(request: Request, res: any) {
   try {
@@ -76,7 +39,7 @@ export async function POST(request: Request, res: any) {
             },
             {
               role: 'system',
-              content: `${role || 'I am a helpful assistant'}. Write with html tags`,
+              content: `${role || 'I am proficient at creating origin stories'}. Write with html tags`,
             },
           ],
         }
@@ -120,3 +83,32 @@ export async function POST(request: Request, res: any) {
 //  * Add limiter to api request by wrapping it around the request
 //  */
 // export default handler;
+
+/**OPENAI-FETCH PACKAGE (NO AXIOS) */
+
+// import { OpenAIClient } from 'openai-fetch';
+
+// export const OpenAIFetch = async (req: any, res: any) => {
+//   const client = new OpenAIClient({ apiKey: process.env.OPENAI_API_KEY });
+//   const method = 'POST';
+
+//   if (req.method === 'POST') {
+//     const { role, characterName, characterClass, age, race, homeland } = await req.json();
+
+//     client.createChatCompletion({
+//       model: 'gpt-3.5-turbo',
+
+//       messages: [
+//         {
+//           role: 'user',
+//           content: `Create a 5 paragraph dungeons and dragons character origin story for ${characterName}, a ${age} year old ${race} ${characterClass} from ${homeland} with html tags `,
+//         },
+//         {
+//           role: 'system',
+//           content: `${role || 'I am a helpful assistant'}. Write with html tags`,
+//         },
+//       ],
+//     });
+//   }
+//   return res.json();
+// };
